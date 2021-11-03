0dfcc1503f8b846fa7c6c6d1f604d1363a86dd40
sedosona <sedosona@kali.sedosona>
2021-11-03 23:06:24 +0200

    remove static

--- Notification_PRO/Notification_Project/settings.py
+++ Notification_PRO/Notification_Project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'notification-front/build/static')
