a1b7a45f9204ed9a696437a47b124d96b4d67658
sedosona <sedosona@kali.sedosona>
2021-11-03 23:13:31 +0200

    add project root

--- Notification_PRO/Notification_Project/settings.py
+++ Notification_PRO/Notification_Project/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'notification-front/build/static')
